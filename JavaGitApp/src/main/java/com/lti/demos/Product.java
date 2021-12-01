bdd5b51f2d777091bf5f1e6fe7bb0f7d3f6f990e
sanjana225 <sanjanams021@gmail.com>
2021-12-01 14:25:16 +0530

    first commit

---
+++ JavaGitApp/src/main/java/com/lti/demos/Product.java
@@ -0,0 +1,41 @@
+package com.lti.demos;
+
+public class Product {
+
+	private int prdId;
+	private String prdName;
+	
+	@Override
+	public String toString() {
+		return "Product [prdId=" + prdId + ", prdName=" + prdName + "]";
+	}
+	
+	public Product(int prdId, String prdName) {
+		super();
+		this.prdId = prdId;
+		this.prdName = prdName;
+	}
+
+	public Product() {
+		super();
+		
+	}
+
+	public int getPrdId() {
+		return prdId;
+	}
+
+	public void setPrdId(int prdId) {
+		this.prdId = prdId;
+	}
+
+	public String getPrdName() {
+		return prdName;
+	}
+
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+	
+	
+}
